e3d3143c6663cde5ea170c143eea5d2e937b14be
Jamshedshoh <jamshedshokh@mail.ru>
2019-11-11 16:09:14 -0500

    Update sketch.ino

--- puzzle-09/sketch/sketch.ino
+++ puzzle-09/sketch/sketch.ino
@@ -5,6 +5,26 @@
 #define LED_PIN 9
 #define LED_COUNT 7
 
+#define PIN_RX 0
+#define PIN_TX 1
+#define PIN_B 2
+#define PIN_485_EN 3
+#define PIN_A 4
+#define PIN_RX1 5
+#define PIN_TX1 6
+#define PIN_LIMIT 7
+#define PIN_NEOPIXEL 9
+#define PIN_SPEAKER 11
+#define PIN_RELAY1 8
+#define PIN_RELAY2 12
+#define PIN_GP1
+#define PIN_GP2 
+#define PIN_GP3
+#define PIN_GP4 
+#define PIN_GP5
+#define PIN_GP6
+#define PIN_GP7
+
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
