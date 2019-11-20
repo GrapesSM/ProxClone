3a526144933c3fe0e94b9f7fb7968482a99264df
Jamshedshoh <jamshedshokh@mail.ru>
2019-11-20 15:06:34 -0500

    create LightGrid class

--- puzzle-09/sketch/sketch.ino
+++ puzzle-09/sketch/sketch.ino
@@ -1,8 +1,5 @@
-#include <Adafruit_NeoPixel.h>
+#include "LightGrid.h"
 #include <ModbusRtu.h>
-#ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
-#endif
 
 #define PIN_RX 0
 #define PIN_TX 1
@@ -27,9 +24,8 @@
 #define LED_COUNT 7
 
 Adafruit_NeoPixel strip(LED_COUNT, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
-
-uint16_t au16data[10] = {
-  3, 127, 127, 127, 127, 0, 0, 0, 127, 0};
+LightGrid lg(&strip, 7);
+uint16_t au16data[10] = {2, 127, 127, 127, 127, 0, 0, 0, 127, 0};
 Modbus slave(1,0,3);
 
 void setup() {
@@ -39,27 +35,21 @@ void setup() {
   #endif
 
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
-  strip.show();            // Turn OFF all pixels ASAP
   strip.setBrightness(255); // Set BRIGHTNESS to about 1/5 (max = 255)
-}
+  strip.show();            // Turn OFF all pixels ASAP
+ }
 
+boolean flag = false;
 void loop() {
   slave.poll( au16data, 10 );
-  for(int i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, strip.Color(au16data[1], au16data[2], au16data[3]));
-    strip.show(); //  Update strip to match
-    delay(50);
+  if (!flag) {
+    flag = true;
+    lg.setColor(au16data[1], au16data[2], au16data[3]);
+    strip.show();  
   }
 
-  for(int i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, strip.Color(au16data[4], au16data[5], au16data[6]));
-    strip.show(); //  Update strip to match
-    delay(50);
-  }
-
-  for(int i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, strip.Color(au16data[7], au16data[8], au16data[9]));
-    strip.show(); //  Update strip to match
-    delay(50);
+  if (au16data[0] != 3) {
+    flag = false;
+    au16data[0] = 3;
   }
 }
