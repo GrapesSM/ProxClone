1e6ea63abdd412e74bb079c0f2993874b350a701
JKCodeIN <jamshedshokh@mail.ru>
2019-11-20 22:55:32 +0000

    Update exercises/modbus-example-3/master/main.py

--- exercises/modbus-example-3/master/main.py
+++ exercises/modbus-example-3/master/main.py
@@ -28,7 +28,7 @@ def main():
         master.set_timeout(5.0)
         master.set_verbose(True)
 
-        logger.info(master.execute(1, cst.READ_HOLDING_REGISTERS, 0, 4))
+        logger.info(master.execute(1, cst.READ_HOLDING_REGISTERS, 0, 10))
 
         #send some queries
         #logger.info(master.execute(1, cst.READ_COILS, 0, 10))
@@ -38,7 +38,7 @@ def main():
         #logger.info(master.execute(1, cst.WRITE_SINGLE_COIL, 7, output_value=1))
         #logger.info(master.execute(1, cst.WRITE_SINGLE_REGISTER, 100, output_value=54))
         #logger.info(master.execute(1, cst.WRITE_MULTIPLE_COILS, 0, output_value=[1, 1, 0, 1, 1, 0, 1, 1]))
-        #logger.info(master.execute(1, cst.WRITE_MULTIPLE_REGISTERS, 0, output_value=[7, 127, 127, 127]))
+        logger.info(master.execute(1, cst.WRITE_MULTIPLE_REGISTERS, 0, output_value=[7, 127, 127, 127, 127,127,127,127,127,127]))
 
     except modbus_tk.modbus.ModbusError as exc:
         logger.error("%s- Code=%d", exc, exc.get_exception_code())
