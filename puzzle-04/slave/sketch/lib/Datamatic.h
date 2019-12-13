f25f71ddb7e3c22384ad0571b0aafb51da90960a
Jamshedshoh <jamshedshokh@mail.ru>
2019-12-13 11:15:15 -0500

    Add initial files

---
+++ puzzle-04/slave/sketch/lib/Datamatic.h
@@ -0,0 +1,35 @@
+/*
+  Datamatic.h - Library for ____.
+*/
+#ifndef Datamatic_h
+#define Datamatic_h
+
+#include <Arduino.h>
+#include "InformationDisplay.h"
+#include "CodeReader.h"
+#include "PowerSwitch.h"
+#include "LightEffect.h"
+#include "Speaker.h"
+
+namespace Datamatic {
+  typedef struct {
+    InformationDisplay informationDisplay;
+    CodeReader codeReader;
+    PowerSwitch powerSwitch;
+    LightEffect lightEffect;
+    Speaker speaker;
+    STATE state;
+  } Components;
+
+  void run(Components c) 
+  {
+    
+  }
+
+  void show(Components c)
+  {
+    
+  }
+}
+
+#endif
