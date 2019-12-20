01549d50553c96b87bd8ace2638f51ed5eff79fc
Jamshedshoh <jamshedshokh@mail.ru>
2019-12-20 10:32:19 -0500

    Add initial logic

--- puzzle-08/slave/sketch/sketch.ino
+++ puzzle-08/slave/sketch/sketch.ino
@@ -1,10 +1,8 @@
 #include "Constants.h"
 #include "lib/ModbusRtu.h"
 #include "NeoPixelBus.h"
-#include <Adafruit_MCP23017.h>
 #include <Adafruit_GFX.h>
 #include "Adafruit_LEDBackpack.h"
-#include <ESP32Encoder.h>
 #include "lib/LifeSupport.h"
 
 struct Puzzle {
@@ -25,9 +23,6 @@ struct Puzzle {
 struct Parts {
   Modbus * slave;
   NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> * strip;
-  Adafruit_MCP23017 mcp1;
-  Adafruit_MCP23017 mcp2;
-  ESP32Encoder encoder;
   Adafruit_7segment matrix = Adafruit_7segment(); 
 } parts;
 
@@ -53,14 +48,6 @@ void setup()
   parts.strip = &strip;
   parts.strip->Begin();
   parts.strip->Show();
-
-  // Setup and Init Encoders
-  ESP32Encoder::useInternalWeakPullResistors=false;
-  //-- adjust starting count value to 0
-  parts.encoder.clearCount();
-  parts.encoder.setCount(0);
-  //-- attach pins for use as encoder pins
-  parts.encoder.attachHalfQuad(PIN_ENCODER_A, PIN_ENCODER_B);
   
   // Setup 7 segment LED
   parts.matrix.begin(ADDR_SEVENSEGMENT);
@@ -69,18 +56,6 @@ void setup()
   pinMode(PIN_SWITCH_1, INPUT);
   pinMode(PIN_SWITCH_2, INPUT);
   pinMode(PIN_SWITCH_3, INPUT);
-
-  // Setup pins for bottom I2C switches
-  parts.mcp1.begin(ADDR_SWITCH_1);
-  for (uint8_t i = 1; i <= NUMBER_OF_SWITCHES_1; i++) {
-    parts.mcp1.pinMode(i, INPUT);
-  }
-
-  // Setup Pins for upper I2C switches
-  parts.mcp2.begin(ADDR_SWITCH_2);
-  for (uint8_t i = 1; i <= NUMBER_OF_SWITCHES_2; i++) {
-    parts.mcp2.pinMode(i, INPUT);
-  }
   
   setupLifeSupport();
 
