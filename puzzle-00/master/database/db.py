2bb7bd820b1bbb4bf615b4bcbe062917c4a367ed
Jamshedshoh <jamshedshokh@mail.ru>
2020-01-23 18:50:01 -0500

    chore

--- puzzle-00/master/database/db.py
+++ puzzle-00/master/database/db.py
@@ -5,7 +5,7 @@ import json
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import configuration as cfg
-print(cfg)
+
 file_path = "".join((os.path.dirname(os.path.abspath(__file__)), "\\" if os.name == "nt" else "/", cfg.db['sqlite']['file']))
 
 db = SqliteDatabase(file_path)
