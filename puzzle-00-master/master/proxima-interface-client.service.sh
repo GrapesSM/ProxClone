bcb7ffcf566b89da048cbea38ecd89998f78bbd3
Jamshedshoh <jamshedshoh.kamolzoda@georgebrown.ca>
2020-12-01 15:39:00 -0500

    Change interface shell scripts

---
+++ puzzle-00-master/master/proxima-interface-client.service.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+DIR_PATH="/home/pi/proxima/puzzle-00-master/master"
+cd "$DIR_PATH/interface/client/"
+serve -s dist
--- puzzle-00-master/master/proxima-interface-server.service.sh
+++ puzzle-00-master/master/proxima-interface-server.service.sh
@@ -1,9 +1,6 @@
 #!/bin/bash 
 
 DIR_PATH="/home/pi/proxima/puzzle-00-master/master"
-cd "$DIR_PATH/interface/client/"
-serve -s dist
-
 cd "$DIR_PATH/interface/server/"
 export FLASK_APP=app.py
 flask run
